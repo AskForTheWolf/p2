f2814b8e67cae4be8af5540bd84a720cde7e08fc
superbuggy <jpqreichard@gmail.com>
2017-06-26 09:53:49 -0400

    add feedback and comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
+
+  # nice use of AR scopes to keep code out of your controllers
   default_scope { order('updated_at DESC') } # sorts by updated at in reverse orderr
+
   has_many :comments
   belongs_to :user
 end
